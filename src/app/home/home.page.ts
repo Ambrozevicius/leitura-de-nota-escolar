cf63bbe2ad3d13b71d7fa08dfff5418d6d1ea7c8
isaac <ipambrozevicius@gmail.com>
2025-02-17 17:23:25 -0300

    nota minima e maxima já estabelecida

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -17,17 +17,25 @@ export class HomePage {
   res=0;
   res_estado='';
 
+  verificacao_inicial() {
+    if (this.n1 > 10 || this.n2 > 10 || this.n3 > 10 || this.n4 > 10 || this.n1 < 0 || this.n2 < 0 || this.n3 < 0 || this.n4 < 0 ) {
+      alert("a nota máxima é 10 e a miníma é 0");
+    } 
+    else {
+      this.calcular();
+    }
+  }
+  
   calcular() {
+
     this.res = (this.n1 + this.n2 + this.n3 + this.n4) / 4;
     
     if (this.res < 3) {
       this.res_estado = "Retido";
-    } 
-    else if(this.res > 6) {
+    } else if (this.res > 6) {
       this.res_estado = "Aprovado";
+    } else { 
+      this.res_estado = "de Recuperação";
     }
-    else{ 
-        this.res_estado = " de Recuperação";
-      }
   }
 }
