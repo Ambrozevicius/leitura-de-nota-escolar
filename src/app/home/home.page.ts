949e7bda0ad4d73f970c9b76a8745c21dc5ff44a
isaac <ipambrozevicius@gmail.com>
2025-02-20 07:44:56 -0300

    maior ou igual recuperação

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,33 +8,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
-  n1=0;
-  n2=0;
-  n3=0;
-  n4=0;
-  res=0;
-  res_estado='';
+  n1 = 0;
+  n2 = 0;
+  n3 = 0;
+  n4 = 0;
+  res = 0;
+  res_estado = '';
 
   verificacao_inicial() {
-    if (this.n1 > 10 || this.n2 > 10 || this.n3 > 10 || this.n4 > 10 || this.n1 < 0 || this.n2 < 0 || this.n3 < 0 || this.n4 < 0 ) {
+    if (this.n1 > 10 || this.n2 > 10 || this.n3 > 10 || this.n4 > 10 || this.n1 < 0 || this.n2 < 0 || this.n3 < 0 || this.n4 < 0) {
       alert("a nota máxima é 10 e a miníma é 0");
-    } 
+    }
     else {
       this.calcular();
     }
   }
-  
+
   calcular() {
 
     this.res = (this.n1 + this.n2 + this.n3 + this.n4) / 4;
-    
+
     if (this.res < 3) {
       this.res_estado = "Retido";
-    } else if (this.res > 6) {
+    } else if (this.res >= 6) {
       this.res_estado = "Aprovado";
-    } else { 
+    } else {
       this.res_estado = "de Recuperação";
     }
   }
